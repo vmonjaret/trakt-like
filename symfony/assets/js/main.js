94fecdd4b2ec401b429a96a74c05baa0ed705d91
vincent monjaret <vmonjaret@gmail.com>
2017-11-03 15:16:03 +0100

    Fixed views
    Signed-off-by: vincent monjaret <vmonjaret@gmail.com>

--- symfony/assets/js/main.js
+++ symfony/assets/js/main.js
@@ -1,2 +1,9 @@
 let $ = require('jquery');
 require("materialize-css/dist/js/materialize");
+
+$('.alert').append('<button class="waves-effect btn-flat close"><i class="material-icons">close</i></button>');
+$('body').on('click', '.alert .close', function() {
+    $(this).parent().fadeOut(300, function() {
+        $(this).remove();
+    });
+});

21fad7678757ca287fcb06c0c4c0d29fff7db240
vincent monjaret <vmonjaret@gmail.com>
2017-11-04 13:12:12 +0100

    added edit profile
    Signed-off-by: vincent monjaret <vmonjaret@gmail.com>

--- symfony/src/AppBundle/Entity/User.php
+++ symfony/src/AppBundle/Entity/User.php
@@ -4,10 +4,13 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="`user`")
+ * @Vich\Uploadable
  */
 class User extends BaseUser
 {
@@ -20,6 +23,24 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @var string
+     * @ORM\Column(name="avatar", type="string", length=255, nullable=true)
+     */
+    private $avatar;
+
+    /**
+     * @var File
+     * @Vich\UploadableField(mapping="user_avatar", fileNameProperty="avatar")
+     */
+    private $avatarFile;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
      * User constructor.
      */
     public function __construct()
@@ -27,4 +48,65 @@ class User extends BaseUser
         parent::__construct();
     }
 
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return User
+     */
+    public function setAvatar( $avatar )
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * @param File $file
+     */
+    public function setAvatarFile( File $file = null )
+    {
+        $this->avatarFile = $file;
+
+        if ($file) {
+            $this->setUpdatedAt();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getAvatarFile()
+    {
+        return $this->avatarFile;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    private function setUpdatedAt()
+    {
+        $this->updatedAt = new \DateTime('now');
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
 }

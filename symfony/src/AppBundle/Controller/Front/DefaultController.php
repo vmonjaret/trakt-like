5018829e0fe204694acc76dfb1f695db0c260e72
haas36u <haasmyriam@yahoo.com>
2017-12-12 16:45:07 +0100

    HomePage : display top 5 most popular movies

--- symfony/src/AppBundle/Controller/Front/DefaultController.php
+++ symfony/src/AppBundle/Controller/Front/DefaultController.php
@@ -15,8 +15,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request, MovieDb $movieDb, MovieManager $movieManager)
     {
+        $movies = $movieDb->getPopular(5);
+
         return $this->render('front/default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'movies' => $movies['movies']
         ]);
     }
 }
